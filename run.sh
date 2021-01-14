ccadbbdf23c721ef9428e7dfd6511908c3b40766
shikishima <160442065@ccalumni.meijo-u.ac.jp>
2021-01-14 17:36:30 +0900

    ビルド済みファイルをイメージから隔離

--- run.sh
+++ run.sh
@@ -13,9 +13,12 @@ docker run \
     --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:ro \
     --volume=$(pwd)/ros-bridge:/home/autoware/ros-bridge:rw \
     --volume=$(pwd)/g29-wheel:/home/autoware/carla_ws/src/g29-wheel:rw \
+    --volume=$(pwd)/ws/build:/home/autoware/carla_ws/build:rw \
+    --volume=$(pwd)/ws/devel:/home/autoware/carla_ws/devel:rw \
     --env="DISPLAY=${DISPLAY}" \
     --privileged \
     --net=host \
+    --name="carla-autoware" \
     $RUNTIME \
     carla-autoware:latest
 
