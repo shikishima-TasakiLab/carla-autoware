95bc61fe151f4d82a31ecebc1c45cb2e19f58c1f
shikishima <160442065@ccalumni.meijo-u.ac.jp>
2021-01-14 23:04:01 +0900

    CARLA本体の同時起動

--- run.sh
+++ run.sh
@@ -8,17 +8,25 @@ else
     RUNTIME="--runtime=nvidia"
 fi
 
-docker run \
-    -it --rm \
-    --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:ro \
-    --volume=$(pwd)/ros-bridge:/home/autoware/ros-bridge:rw \
-    --volume=$(pwd)/g29-wheel:/home/autoware/carla_ws/src/g29-wheel:rw \
-    --volume=$(pwd)/ws/build:/home/autoware/carla_ws/build:rw \
-    --volume=$(pwd)/ws/devel:/home/autoware/carla_ws/devel:rw \
-    --env="DISPLAY=${DISPLAY}" \
-    --privileged \
-    --net=host \
-    --name="carla-autoware" \
-    $RUNTIME \
-    carla-autoware:latest
+gnome-terminal -- \
+    docker run \
+        -it --rm \
+        --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:ro \
+        --volume=$(pwd)/ros-bridge:/home/autoware/ros-bridge:rw \
+        --volume=$(pwd)/g29-wheel:/home/autoware/carla_ws/src/g29-wheel:rw \
+        --volume=$(pwd)/ws/build:/home/autoware/carla_ws/build:rw \
+        --volume=$(pwd)/ws/devel:/home/autoware/carla_ws/devel:rw \
+        --env="DISPLAY=${DISPLAY}" \
+        --privileged \
+        --net=host \
+        --name="carla-autoware" \
+        $RUNTIME \
+        carla-autoware:latest
 
+gnome-terminal -- \
+    docker run \
+        -it --rm \
+        -p 2000-2002:2000-2002 \
+        --gpus all \
+        carlasim/carla:0.9.10.1 \
+        bash -c "DISPLAY= SDL_VIDEODRIVER=offscreen SDL_HINT_CUDA_DEVICE=0 ./CarlaUE4.sh -opengl"
