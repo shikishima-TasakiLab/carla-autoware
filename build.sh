c137414b32f751afaaad7e7daaa2e226aed469f2
shikishima-TasakiLab <160442065@ccalumni.meijo-u.ac.jp>
2021-01-20 22:50:50 +0900

    CARLAのバージョンを指定

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
-docker build -t carla-autoware -f Dockerfile . "$@"
+#!/bin/bash
+CARLA_VERSION="0.9.10.1"
 
+BUILD_DIR=$(dirname $(readlink -f $0))
+
+docker build -t carla-autoware:${CARLA_VERSION} -f Dockerfile ${BUILD_DIR}
--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+CARLA_VERSION="0.9.10.1"
+
+RUN_DIR=$(dirname $(readlink -f $0))
 
 RUNTIME=""
 DOCKER_VERSION=$(docker version --format '{{.Client.Version}}' | cut --delimiter=. --fields=1,2)
@@ -11,22 +14,23 @@ fi
 gnome-terminal -- \
     docker run \
         -it --rm \
-        --volume=$(pwd)/autoware-contents:/home/autoware/autoware-contents:ro \
-        --volume=$(pwd)/ros-bridge:/home/autoware/ros-bridge:rw \
-        --volume=$(pwd)/g29-wheel:/home/autoware/carla_ws/src/g29-wheel:rw \
-        --volume=$(pwd)/ws/build:/home/autoware/carla_ws/build:rw \
-        --volume=$(pwd)/ws/devel:/home/autoware/carla_ws/devel:rw \
-        --env="DISPLAY=${DISPLAY}" \
-        --privileged \
-        --net=host \
-        --name="carla-autoware" \
-        $RUNTIME \
-        carla-autoware:latest
+        -p 2000-2002:2000-2002 \
+        ${RUNTIME} \
+        carlasim/carla:${CARLA_VERSION} \
+        bash -c "DISPLAY= SDL_VIDEODRIVER=offscreen SDL_HINT_CUDA_DEVICE=0 ./CarlaUE4.sh -opengl"
 
 gnome-terminal -- \
     docker run \
         -it --rm \
-        -p 2000-2002:2000-2002 \
-        --gpus all \
-        carlasim/carla:0.9.10.1 \
-        bash -c "DISPLAY= SDL_VIDEODRIVER=offscreen SDL_HINT_CUDA_DEVICE=0 ./CarlaUE4.sh -opengl"
+        --volume=${RUN_DIR}/autoware-contents:/home/autoware/autoware-contents:ro \
+        --volume=${RUN_DIR}/ros-bridge:/home/autoware/ros-bridge:rw \
+        --volume=${RUN_DIR}/g29-wheel:/home/autoware/carla_ws/src/g29-wheel:rw \
+        --volume=${RUN_DIR}/ws/build:/home/autoware/carla_ws/build:rw \
+        --volume=${RUN_DIR}/ws/devel:/home/autoware/carla_ws/devel:rw \
+        --volume=${RUN_DIR}/shared_dir:/home/autoware/shared_dir:rw \
+        --env="DISPLAY=${DISPLAY}" \
+        --privileged \
+        --net=host \
+        --name="carla-autoware" \
+        ${RUNTIME} \
+        carla-autoware:${CARLA_VERSION}
